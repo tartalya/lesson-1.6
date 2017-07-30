b0e950bfa67e76f48525084a6cab5a98e9c57adb
saha <saha@DESKTOP-D7M35T3>
2017-07-31 00:50:01 +0300

    imagestring vs imagettfstring

--- sert.php
+++ sert.php
@@ -5,6 +5,7 @@ $string = $_GET['string'];
 $im = imagecreatefrompng("sert.png");
 $color = imagecolorallocate($im, 0, 0, 0);
 imagettftext($im, 20, 0, 90, 280, $color, 'arial.ttf', $string);
+//imagestring($im, 3, 90, 319, $string, $color);
 imagepng($im);
 imagedestroy($im);
 ?>
