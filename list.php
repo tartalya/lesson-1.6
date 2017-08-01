3df63aa4e496c8ad03cdf71fcd87776dfc9ab682
saha-work <brutal4028@gmail.com>
2017-08-01 17:07:43 +0300

    login db extend list.php cleanup

--- list.php
+++ list.php
@@ -1,31 +1,5 @@
 <?php
 
-/*
-  $test_primer = array(
-
-  array ('questions' => 'Сколько будет 2х2',
-  'variants' => array('5', '3', '4'),
-  'right' => array('4')),
-
-  array ('questions' => 'Сколько будет 2х8',
-  'variants' => array('19', '56', '16'),
-  'right' => array('16')
-
-
-  ));
-
-
-
-  echo json_encode($test_primer);
-
-  echo '<pre>';
-  print_r($test_primer);
- */
-?>
-
-
-<?php
-
 error_reporting(E_ALL & ~E_NOTICE);
 
 if (!json_decode(file_get_contents('list.db'), true)) {
