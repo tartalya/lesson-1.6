fedd49ec169de0d2e92069674e49de1a30547759
saha-work <brutal4028@gmail.com>
2017-08-02 12:17:03 +0300

    logout + fixes

--- logout.php
+++ logout.php
@@ -6,8 +6,9 @@ setcookie('user_id', "", time() - 10800);
 setcookie('name', "", time() - 10800);
 setcookie('guest_name', "", time() - 10800);
 
+if (isset($_SERVER['HTTP_REFERER'])) {
 header('Location:' . $_SERVER['HTTP_REFERER']);
-
+}
 
 
 ?>
